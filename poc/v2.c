3a9e07812c470e34e2d037d7ba912078b117d737
Valentin LEPILLER <vlepille@student.42.fr>
2023-05-13 19:20:57 +0200

    Add v2 of poc

--- poc/v2.c
+++ poc/v2.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
@@ -16,244 +17,266 @@ if (arg) \
 	exit(EXIT_FAILURE); \
 }
 
-typedef struct s_redir
+typedef struct s_string
 {
-	char	*arg;
-	enum	{PIPE, STD_IO} type;
-	char	append;
-}	t_redir;
+	char	*addr;
+	int		len;
+}	t_string;
 
-typedef struct s_cmd
+#define NOT_FOUND -1
+#define DEBUG 0
+#define WITH_PARENTHESIS_FORK 0
+
+int	has(char *str, int len, char *substr)
 {
-	char	*path;
-	char	**argv;
-	char	**envp; // @TODO ? rm
-	t_redir	in;
-	t_redir	out;
-}	t_cmd;
+	int	parenthesis = 0;
+	unsigned long j;
 
-typedef struct s_tree_node t_tree_node;
-struct s_tree_node
-{
-	char		*str;
-	t_tree_node	*left;
-	t_tree_node	*right;
-	enum		{NONE, PIPED, AND, OR} type;
-};
-
-void	child(int pipefd[2], int precedent_fd, t_cmd cmd)
-{
-	int	fd;
-
-	// INPUT REDIR
-	if (cmd.in.arg)
+	for (int i = len - 1; i >= 0; i--)
 	{
-		test_error((fd = open(cmd.in.arg, O_RDONLY)) == -1);
-		test_error(dup2(fd, STDIN_FILENO) == -1);
-		test_error(close(fd) == -1);
+		if ((parenthesis += (str[i] == '(') - (str[i] == ')')) == 0)
+		{
+			for (j = 0; j < strlen(substr); j++)
+			{
+				if (str[i + j] != substr[j])
+					break;
+			}
+			if (!substr[j])
+				return (i);
+		}
 	}
-	else if (cmd.in.type == PIPE)
-		test_error(dup2(precedent_fd, STDIN_FILENO) == -1);
-	if (cmd.in.type == PIPE)
-		test_error(close(precedent_fd) == -1);
-
-	// OUTPUT REDIR
-	if (cmd.out.arg)
-	{
-		test_error((fd = open(cmd.out.arg, O_WRONLY | O_CREAT | (O_APPEND * (cmd.out.append)) | (O_TRUNC * (!cmd.out.append)), S_IRWXU | S_IRGRP | S_IXGRP | S_IXOTH)) == -1);
-		test_error(dup2(fd, STDOUT_FILENO) == -1);
-		test_error(close(fd) == -1);
-	}
-	else if (cmd.out.type == PIPE)
-		test_error(dup2(pipefd[1], STDOUT_FILENO) == -1);
-	if (cmd.out.type == PIPE)
-		test_error(close(pipefd[1]) == -1);
-
-	// execve
-	test_error(execve(cmd.path, cmd.argv, cmd.envp) == -1);
+	return (NOT_FOUND);
 }
 
-void	exec(t_cmd	*cmds, int cmds_len)
+char	*concat(t_string cmd)
 {
-	int		pipefd[2] = {-1, -1};
-	int		precedent_fd;
-	int		current_status;
-	int		*cpids;
-	int		i;
-	int		j;
+	char	*res = malloc(cmd.len + 1 + strlen("/bin/"));
+	unsigned long		i;
 
-	test_error((cpids = malloc(sizeof(int) * cmds_len)) < 0);
 	i = 0;
-	while (i < cmds_len)
+	while (i < strlen("/bin/"))
 	{
-		if ((*cmds).in.type != PIPE && (*cmds).out.type != PIPE)
-		{
-			cpids[i] = fork(); // @TODO in minishell : if builtin, don't fork
-			test_error(cpids[i] == -1)
-			if (!cpids[i])
-				child((int [2]){-1, -1}, -1, *cmds);
-			waitpid(cpids[i], &current_status, 0);
-		}
-		if ((*cmds).in.type != PIPE && (*cmds).out.type == PIPE)
-		{
-			j = i;
-			test_error(pipe(pipefd) == -1)
-			cpids[i] = fork();
-			if (!cpids[i])
-				child(pipefd, -1, (*cmds));
-			test_error(close(pipefd[1]) == -1);
-			precedent_fd = pipefd[0];
-		}
-		if ((*cmds).in.type == PIPE && (*cmds).out.type == PIPE)
-		{
-			test_error(pipe(pipefd) == -1)
-			cpids[i] = fork();
-			if (!cpids[i])
-				child(pipefd, precedent_fd, (*cmds));
-			test_error(close(precedent_fd) == -1);
-			test_error(close(pipefd[1]) == -1);
-			precedent_fd = pipefd[0];
-		}
-		if ((*cmds).in.type == PIPE && (*cmds).out.type != PIPE)
-		{
-			cpids[i] = fork();
-			if (!cpids[i])
-				child((int [2]){-1, -1}, precedent_fd, (*cmds));
-			test_error(close(precedent_fd) == -1);
-			while (j < i)
-				waitpid(cpids[j++], &current_status, 0);
-		}
-		//if ((*cmds).in.type != PIPE)
-		//	precedent_fd = -1;
-		//if ((*cmds).in.type == PIPE || (*cmds).out.type == PIPE)
-		//	test_error(pipe(pipefd) == -1)
-		//cpids[i] = fork(); // @TODO in minishell : if builtin, don't fork if not in a pipe
-		//test_error(cpids[i] == -1)
-		//if (!cpids[i])
-		//	child(pipefd, precedent_fd, (*cmds));
-		//if ((*cmds).in.type == PIPE)
-		//	test_error(close(precedent_fd) == -1);
-		//if ((*cmds).in.type == PIPE || (*cmds).out.type == PIPE)
-		//{
-		//	test_error(close(pipefd[1]) == -1);
-		//	precedent_fd = pipefd[0];
-		//}
-		cmds++;
+		res[i] = "/bin/"[i];
 		i++;
 	}
-	//i = 0;
-	//while (i < cmds_len)
-	//{
-	//	waitpid(cpids[i++], &current_status, 0); // @TODO use waitpid
-	//	//printf("cpid[%d] == %d -> %d\n", i - 1, cpids[i - 1], current_status);
-	//}
-	free(cpids);
+	while (i < cmd.len + strlen("/bin/"))
+	{
+		res[i] = cmd.addr[i - strlen("/bin/")];
+		i++;
+	}
+	res[i] = 0;
+	return (res);
 }
 
-int	first_test(char *envp[], char *infile, char *outfile)
+char	*str_to_charp(t_string str)
+{
+	char	*res = malloc(str.len + 1);
+	unsigned long		i;
+
+	if (str.len < 0)
+		return (NULL);
+	i = -1;
+	while (++i < (unsigned long)str.len)
+		res[i] = str.addr[i];
+	res[i] = 0;
+	return (res);
+}
+
+int	exec_cmd(char *str, int len, char *envp[])
+{
+	t_string	*current;
+	t_string	cmd;
+	t_string	arg;
+	t_string	redir[2];
+	int			in_redir = 0;
+	int			word_start = -1;
+	int			append;
+
+	current = &cmd;
+	arg.len = -1;
+	redir[0].len = -1;
+	redir[1].len = -1;
+	for (int i = 0; i < len; i++)
+	{
+		if (word_start == -1 && str[i] != ' ')
+		{
+			if (str[i] == '>')
+			{
+				append = str[i + 1] == '>';
+				in_redir = 2;
+			}
+			else if (str[i] == '<')
+				in_redir = 1;
+			else
+				word_start = i;
+		}
+		if (word_start > -1 && (str[i] == ' ' || i + 1 == len))
+		{
+			if (in_redir == 1)
+			{
+				current = &redir[0];
+				in_redir = 0;
+			}
+			else if (in_redir == 2)
+			{
+				current = &redir[2];
+				in_redir = 0;
+			}
+			current->addr = str + word_start;
+			current->len = i - word_start + (str[i] != ' ');
+			current = &arg;
+			word_start = -1;
+		}
+	}
+
+	// not below if builtin
+	char *path = concat(cmd);
+	int	cpid;
+	test_error((cpid = fork()) == -1);
+	if (!cpid)
+	{
+		int	fd1, fd2;
+		if (redir[0].len >= 0)
+		{
+			test_error((fd1 = open(str_to_charp(redir[0]), O_RDONLY)) == -1);
+			dup2(fd1, STDIN_FILENO);
+		}
+		if (redir[0].len >= 0)
+		{
+			test_error((fd2 = open(str_to_charp(redir[0]), O_WRONLY | O_CREAT | (O_APPEND * append) | (O_TRUNC * !append), S_IRWXU | S_IRGRP | S_IXGRP | S_IXOTH)) == -1);
+			dup2(fd2, STDOUT_FILENO);
+		}
+		if (DEBUG)
+			printf("<%s><%s><%s>\n", path, str_to_charp(cmd), str_to_charp(arg));
+		test_error((execve(path, (char *[]){str_to_charp(cmd), str_to_charp(arg), NULL}, envp)) == -1);
+	}
+	int	res;
+	waitpid(cpid, &res, 0);
+	return (res);
+}
+
+int	pip(char *str, int len, char *envp[])
+{
+	int	pipefd[2];
+	int	cmds_len = 0;
+	int	*cpids;
+	int	precedent_fd;
+	int	res;
+	int	start = 0;
+	int	j;
+
+	for (int i = 0; i < len; i++)
+		cmds_len += str[i] == '|';
+	test_error(!(cpids = malloc(sizeof(int) * cmds_len)));
+	test_error(pipe(pipefd) == -1)
+	cpids[0] = fork();
+	test_error(close(pipefd[0]) == -1);
+	if (!cpids[0])
+	{
+		test_error(dup2(pipefd[1], STDOUT_FILENO) == -1);
+		test_error(close(pipefd[1]) == -1);
+		res = exec_cmd(str, has(str, len, "|"), envp);
+	}
+	test_error(close(pipefd[1]) == -1);
+	precedent_fd = pipefd[0];
+	j = 0;
+	while ((start = has(str + start, len - start, "|") + 1) != -1)
+	{
+		j++;
+		if (has(str + start, len - start, "|") != -1)
+		{
+			test_error(pipe(pipefd) == -1)
+			cpids[j] = fork();
+			if (!cpids[j])
+			{
+				test_error(dup2(precedent_fd, STDIN_FILENO) == -1);
+				test_error(close(precedent_fd) == -1);
+				test_error(dup2(pipefd[1], STDOUT_FILENO) == -1);
+				test_error(close(pipefd[1]) == -1);
+				res = exec_cmd(str + start, has(str + start, len - start, "|"), envp);
+			}
+			test_error(close(precedent_fd) == -1);
+			test_error(close(pipefd[1]) == -1);
+			precedent_fd = pipefd[0];
+		}
+		else
+		{
+			cpids[j] = fork();
+			if (!cpids[j])
+			{
+				test_error(dup2(precedent_fd, STDIN_FILENO) == -1);
+				test_error(close(precedent_fd) == -1);
+				res = exec_cmd(str + start, len - start, envp);
+			}
+			test_error(close(precedent_fd) == -1);
+		}
+	}
+	for (int i = 0; i < cmds_len; i++)
+		waitpid(cpids[i], &res, 0);
+	return (res);
+}
+
+int	exec_tree(char *str, int len, char *envp[])
 {
 	int		i;
-	t_cmd	*cmds;
-	int		cmds_len = 9; // !!!!!!!!!!!!!!!!!!!!!!!!!
+	int		j;
+	int		ret;
 
+	if (DEBUG)
+		printf("<%s> len = %d\n", str, len);
 	i = 0;
-	test_error((cmds = malloc(sizeof(t_cmd) * cmds_len)) < 0);
-	cmds[i++] = (t_cmd){"/bin/ls", (char *[]){"ls", NULL}, envp, (t_redir){infile, STD_IO, 0}, (t_redir){NULL, STD_IO, 0}};
-	cmds[i++] = (t_cmd){"/bin/echo", (char *[]){"echo", "a", NULL}, envp, (t_redir){NULL, STD_IO, 0}, (t_redir){NULL, STD_IO, 0}};
-	cmds[i++] = (t_cmd){"/bin/ls", (char *[]){"ls", NULL}, envp, (t_redir){infile, STD_IO, 0}, (t_redir){NULL, PIPE, 0}};
-	cmds[i++] = (t_cmd){"/bin/cat", (char *[]){"cat", NULL}, envp, (t_redir){NULL, PIPE, 0}, (t_redir){NULL, PIPE, 0}};
-	cmds[i++] = (t_cmd){"/bin/wc", (char *[]){"wc", "-l", NULL}, envp, (t_redir){NULL, PIPE, 0}, (t_redir){outfile, PIPE, 1}};
-	cmds[i++] = (t_cmd){"/bin/grep", (char *[]){"grep", NULL}, envp, (t_redir){NULL, PIPE, 0}, (t_redir){NULL, STD_IO, 0}};
-	cmds[i++] = (t_cmd){"/bin/echo", (char *[]){"echo", "b", NULL}, envp, (t_redir){NULL, STD_IO, 0}, (t_redir){NULL, STD_IO, 0}};
-	cmds[i++] = (t_cmd){"/bin/echo", (char *[]){"echo", "c", NULL}, envp, (t_redir){NULL, STD_IO, 0}, (t_redir){NULL, PIPE, 0}};
-	cmds[i++] = (t_cmd){"/bin/cat", (char *[]){"cat", NULL}, envp, (t_redir){NULL, PIPE, 0}, (t_redir){NULL, STD_IO, 0}};
-	assert(i == cmds_len);
-
-	exec(cmds, cmds_len);
-	free(cmds);
-	return (0);
+	while (str[i] == ' ')
+		i++;
+	if (str[i] == '(')
+	{
+		j = len - 1;
+		while (j > i)
+		{
+			if (str[j] == ')')
+			{
+				len = j - i - 1;
+				break ;
+			}
+			j--;
+		}
+		str = str + i + 1;
+		if (DEBUG)
+			printf("Parenthesis<%s> len = %d\n", str, len);
+		if (WITH_PARENTHESIS_FORK)
+		{
+			int cpid = fork();
+			test_error(cpid == -1)
+			if (!cpid)
+				exit(exec_tree(str, len, envp));
+			waitpid(cpid, &ret, 0);
+			return (ret);
+		}
+		else
+			return (exec_tree(str, len, envp));
+	}
+	if ((i = has(str, len, "&&")) != NOT_FOUND)
+	{
+		if ((ret = exec_tree(str, i, envp)) == EXIT_SUCCESS)
+			return (exec_tree(str + i + 2, len - (i + 2), envp));
+		return (ret);
+	}
+	if ((i = has(str, len, "||")) != NOT_FOUND)
+	{
+		if ((ret = exec_tree(str, i, envp)) != EXIT_SUCCESS)
+			return (exec_tree(str + i + 2, len - (i + 2), envp));
+		return (ret);
+	}
+	if (has(str, len, "|") != NOT_FOUND)
+	{
+		return (pip(str, len, envp));
+	}
+	return (exec_cmd(str, len, envp));
 }
-
-int	second_test(char *envp[], char *infile, char *outfile)
-{
-	int		i;
-	t_cmd	*cmds;
-	int		cmds_len = 4; // !!!!!!!!!!!!!!!!!!!!!!!!!
-
-	(void)infile;
-	(void)outfile;
-	i = 0;
-	test_error((cmds = malloc(sizeof(t_cmd) * cmds_len)) < 0);
-	cmds[i++] = (t_cmd){"/bin/ls", (char *[]){"ls", NULL}, envp, (t_redir){NULL, STD_IO, 0}, (t_redir){NULL, PIPE, 0}};
-	cmds[i++] = (t_cmd){"/bin/cat", (char *[]){"cat", NULL}, envp, (t_redir){NULL, PIPE, 0}, (t_redir){NULL, PIPE, 0}};
-	cmds[i++] = (t_cmd){"/bin/wc", (char *[]){"wc", "-l", NULL}, envp, (t_redir){NULL, PIPE, 0}, (t_redir){NULL, PIPE, 0}};
-	cmds[i++] = (t_cmd){"/bin/grep", (char *[]){"grep", NULL}, envp, (t_redir){NULL, PIPE, 0}, (t_redir){NULL, STD_IO, 0}};
-	assert(i == cmds_len);
-
-	exec(cmds, cmds_len);
-	free(cmds);
-	return (0);
-}
-
-int	third_test(char *envp[], char *infile, char *outfile)
-{
-	int		i;
-	t_cmd	*cmds;
-	int		cmds_len = 4; // !!!!!!!!!!!!!!!!!!!!!!!!!
-
-	(void)infile;
-	(void)outfile;
-	i = 0;
-	test_error((cmds = malloc(sizeof(t_cmd) * cmds_len)) < 0);
-	cmds[i++] = (t_cmd){"/bin/ls", (char *[]){"ls", NULL}, envp, (t_redir){NULL, STD_IO, 0}, (t_redir){NULL, PIPE, 0}};
-	cmds[i++] = (t_cmd){"/bin/cat", (char *[]){"cat", NULL}, envp, (t_redir){NULL, PIPE, 0}, (t_redir){NULL, PIPE, 0}};
-	cmds[i++] = (t_cmd){"/bin/grep", (char *[]){"grep", "e", NULL}, envp, (t_redir){NULL, PIPE, 0}, (t_redir){NULL, PIPE, 0}};
-	cmds[i++] = (t_cmd){"/bin/wc", (char *[]){"wc", "-l", NULL}, envp, (t_redir){NULL, PIPE, 0}, (t_redir){NULL, STD_IO, 0}};
-	assert(i == cmds_len);
-
-	exec(cmds, cmds_len);
-	free(cmds);
-	return (0);
-}
-
-//int	main(int argc, char *argv[], char *envp[])
-//{
-//	char	*infile;
-//	char	*outfile;
-//	int		test_num;
-
-//	//setbuf(stdout, NULL);
-//	if (argc < 3 || argc > 4)
-//	{
-//		infile = "infile";
-//		outfile = "outfile";
-//	}
-//	else
-//	{
-//		infile = argv[1];
-//		outfile = argv[2];
-//	}
-//	if (argc == 2)
-//		test_num = atoi(argv[1]);
-//	else if (argc == 4)
-//		test_num = atoi(argv[3]);
-//	else
-//		test_num = -1;
-
-//	if (test_num == -1 || test_num == 1)
-//		first_test(envp, infile, outfile);
-//	if (test_num == -1 || test_num == 2)
-//		second_test(envp, infile, outfile);
-//	if (test_num == -1 || test_num == 3)
-//		third_test(envp, infile, outfile);
-//}
-
-int	exec_tree()
 
 int	main(int argc, char *argv[], char *envp[])
 {
 	if (argc != 2)
 		return (EXIT_FAILURE);
 
-	return (exec_tree(argv[1]));
+	return (exec_tree(argv[1], strlen(argv[1]), envp));
 }
