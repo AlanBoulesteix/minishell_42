bdb3ab198966cca923b92ddb4b44557cb162edd4
Valentin LEPILLER <vlepille@student.42.fr>
2023-05-15 17:43:35 +0200

    Add error handling

---
+++ src/error.c
@@ -0,0 +1,22 @@
+#include "minishell.h"
+
+void	error(int errno)
+{
+	if (errno == MALLOC_FAIL_ERRNO)
+		perror("Malloc :");
+	else if (errno == FORK_FAIL_ERRNO)
+		perror("Fork :");
+	else if (errno == EXECVE_FAIL_ERRNO)
+		perror("Execve :");
+	else if (errno == PIPE_FAIL_ERRNO)
+		perror("Pipe :");
+	else if (errno == OPEN_FAIL_ERRNO)
+		perror("Open :");
+	else if (errno == CLOSE_FAIL_ERRNO)
+		perror("Close :");
+	else if (errno == DUP2_FAIL_ERRNO)
+		perror("Close :");
+	else if (errno > 0)
+		perror("Error handling not implementated yet :");
+	exit(errno);
+}
