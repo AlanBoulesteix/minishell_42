130b208accf515fc57f826e56ce56b5900229f1a
Valentin LEPILLER <vlepille@student.42.fr>
2023-06-01 11:16:12 +0200

    Change the error message for cd for too many arguments

--- src/builtin_cd.c
+++ src/builtin_cd.c
@@ -50,6 +50,6 @@ int	cd_cmd(char **args, t_context *context, int input_fd, int output_fd)
 	(void)input_fd;
 	(void)output_fd;
 	if (!args[0] || args[1])
-		return ((write(STDERR_FILENO, "bash: cd: too many arguments\n", 29)), 1);
+		return ((write(STDERR_FILENO, "minishell: cd: too many arguments\n", 29)), 1);
 	return (cd(args[0], context));
 }
