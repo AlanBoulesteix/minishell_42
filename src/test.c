31c567d97c3e30bce7c95e0dc3587aba1a846c42
AlanBoulesteix <alan.boulesteix2312@gmail.com>
2023-06-11 20:07:18 +0200

    Begin to implement Heredoc

--- src/test.c
+++ src/test.c
@@ -1,4 +1,19 @@
-int test(void)
+#include "minishell.h"
+#include <unistd.h>
+
+
+int heredoc(char *str)
 {
-    return 1;
+    int     end[2];
+    int     count;
+    char    *line;
+    char    buffer[1];
+
+    line = "start";
+    count = 1;
+    while (count && line && ft_streq(line, str))
+    {
+        count = read(STDIN_FILENO, buffer, 1);
+        line = join_char(line, buffer);
+    }
 }
