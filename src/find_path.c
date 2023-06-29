be35e760fe470b0e1a0398f39f52911cca6b0567
Valentin LEPILLER <vlepille@student.42.fr>
2023-06-29 18:00:56 +0200

    Norm find_path

--- src/find_path.c
+++ src/find_path.c
@@ -6,7 +6,7 @@
 /*   By: vlepille <vlepille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 12:19:28 by aboulest          #+#    #+#             */
-/*   Updated: 2023/06/23 03:01:18 by vlepille         ###   ########.fr       */
+/*   Updated: 2023/06/29 17:58:06 by vlepille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,19 +44,13 @@ char	**ft_path(t_env *env)
 	return (final_path);
 }
 
-int	is_file(char *path)
+static char	*find_path_in_env(char *command, t_env *env)
 {
 	struct stat	buf;
-
-	return (!(stat(path, &buf) == 0 && S_ISDIR(buf.st_mode)));
-}
-
-char	*find_path_in_env(char *command, t_env *env)
-{
-	char	*final_path;
-	char	**path;
-	char	*add_slash;
-	int		i;
+	char		*final_path;
+	char		**path;
+	char		*add_slash;
+	int			i;
 
 	path = ft_path(env);
 	if (!path)
@@ -67,7 +61,8 @@ char	*find_path_in_env(char *command, t_env *env)
 		add_slash = ft_strjoin(path[i], "/");
 		final_path = ft_strjoin(add_slash, command);
 		free(add_slash);
-		if (access(final_path, F_OK) == 0 && is_file(final_path))
+		if (access(final_path, F_OK) == 0
+			&& (!(stat(final_path, &buf) == 0 && S_ISDIR(buf.st_mode))))
 			return (free_db_tab(path), final_path);
 		if (final_path)
 			free(final_path);
@@ -76,10 +71,36 @@ char	*find_path_in_env(char *command, t_env *env)
 	return (NULL);
 }
 
-char	*find_path(char *command, t_context *context)
+static char	*find_slash_path(char *command, t_context *context)
 {
 	struct stat	buf;
 
+	if (stat(command, &buf) == 0 && S_ISDIR(buf.st_mode))
+	{
+		printf_fd(STDERR_FILENO, "minishell: %s: Is a directory\n", command);
+		context->exit_value = 126;
+		return (NULL);
+	}
+	if (access(command, X_OK) == 0)
+		return (ft_strdup(command));
+	else if (access(command, F_OK) == 0)
+	{
+		printf_fd(
+			STDERR_FILENO, "minishell: %s: %s\n", command, strerror(errno));
+		context->exit_value = 126;
+		return (ft_strdup(command));
+	}
+	else
+	{
+		printf_fd(
+			STDERR_FILENO, "minishell: %s: %s\n", command, strerror(errno));
+		context->exit_value = 127;
+		return (NULL);
+	}
+}
+
+char	*find_path(char *command, t_context *context)
+{
 	if (command == NULL || command[0] == '\0')
 		return (NULL);
 	if (command[0] == '.' && !command[1])
@@ -89,28 +110,6 @@ char	*find_path(char *command, t_context *context)
 		return (NULL);
 	}
 	if (ft_strchr(command, '/') != 0)
-	{
-		if (stat(command, &buf) == 0 && S_ISDIR(buf.st_mode))
-		{
-			printf_fd(STDERR_FILENO, "minishell: %s: Is a directory\n", command);
-			context->exit_value = 126;
-			return (NULL);
-		}
-		if (access(command, X_OK) == 0)
-			return (ft_strdup(command));
-		else if (access(command, F_OK) == 0)
-		{
-			printf_fd(
-				STDERR_FILENO, "minishell: %s: %s\n", command, strerror(errno));
-			context->exit_value = 126;
-			return (ft_strdup(command));
-		}
-		else
-		{
-			printf_fd(
-				STDERR_FILENO, "minishell: %s: %s\n", command, strerror(errno));
-			context->exit_value = 127;
-		}
-	}
+		return (find_slash_path(command, context));
 	return (find_path_in_env(command, &context->env));
 }
